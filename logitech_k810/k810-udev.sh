bfd44ab66095172ae9c6ed5e724fce92161fc6e2
Kalman Olah <hello@kalmanolah.net>
2015-08-12 13:32:57 +0200

    Add logitech k810 stuff
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

---
+++ logitech_k810/k810-udev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# LOGFILE=/tmp/logfilek810sh.log
+# echo "RUN: at `date` by `whoami` act $ACTION \$1 $1 DEVPATH $DEVPATH DEVNAME $DEVNAME" >> ${LOGFILE}
+# echo "Setting F-keys on for your K810!"
+
+if [ "$ACTION" == "add" ];
+then
+    # configure k810 ($DEVPATH) at $DEVNAME.
+    # /opt/bin/k810_conf -d $DEVNAME -f on
+
+    # xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+    /opt/logitech-k810/k810_conf -d $DEVNAME -f on
+fi
