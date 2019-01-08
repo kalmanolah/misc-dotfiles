8205b17d0f30f1660fbff9ef963b230ad5e7d436
Kalman Olah <kalman@inuits.eu>
2019-01-08 16:42:48 +0100

    update
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- logitech_k810/k810-udev.sh
+++ logitech_k810/k810-udev.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 LOGFILE=/tmp/logfilek810sh.log
 echo "RUN: at `date` by `whoami` act $ACTION \$1 $1 DEVPATH $DEVPATH DEVNAME $DEVNAME" >> ${LOGFILE}
 echo "Setting F-keys on for your K810!"
-HIDDEV="/dev/$(ls $(dirname $(dirname /sys/${DEVPATH}))/hidraw)"
 
 if [ "$ACTION" == "add" ];
 then
     # configure k810 ($DEVPATH) at $DEVNAME.
-    # /opt/bin/k810_conf -d $DEVNAME -f on
-
-    # xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
-    /opt/logitech-k810/k810_conf -d $HIDDEV -f on
+    /opt/logitech-k810/k810_conf -d $DEVNAME -f on
 fi
