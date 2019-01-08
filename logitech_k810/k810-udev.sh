e5e155e06391d92794399d6430944091df223a6d
Kalman Olah <kalman@inuits.eu>
2019-01-08 17:26:15 +0100

    update
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- logitech_k810/k810-udev.sh
+++ logitech_k810/k810-udev.sh
@@ -5,7 +5,6 @@ echo "Setting F-keys on for your K810!"
 
 if [ "$ACTION" == "add" ];
 then
-    /usr/bin/setxkbmap be
     # configure k810 ($DEVPATH) at $DEVNAME
     /opt/logitech-k810/k810_conf -d $DEVNAME -f on
 fi
