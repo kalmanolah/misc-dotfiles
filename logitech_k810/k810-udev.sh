69daa419bc648b242e2b883d9b6a67558598dac6
Kalman Olah <kalman@inuits.eu>
2019-01-08 17:23:03 +0100

    update
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- logitech_k810/k810-udev.sh
+++ logitech_k810/k810-udev.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 LOGFILE=/tmp/logfilek810sh.log
-echo "RUN: at `date` by `whoami` act $ACTION \$1 $1 DEVPATH $DEVPATH DEVNAME $DEVNAME" >> ${LOGFILE}
+echo "RUN: at `date` by `whoami` act $ACTION DEVNAME $DEVNAME" >> ${LOGFILE}
 echo "Setting F-keys on for your K810!"
 
 if [ "$ACTION" == "add" ];
 then
-    # configure k810 ($DEVPATH) at $DEVNAME.
+    /usr/bin/setxkbmap be
+    # configure k810 ($DEVPATH) at $DEVNAME
     /opt/logitech-k810/k810_conf -d $DEVNAME -f on
 fi
